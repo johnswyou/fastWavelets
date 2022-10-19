db4834fcd034e6880276dde25e7a946be31d9de0
John You <johnswyou@gmail.com>
2022-10-19 12:53:24 -0400

    Add documentation for shape_check

--- R/shape_check.R
+++ R/shape_check.R
@@ -1,3 +1,12 @@
+#' @title Check Matrix X is (N x 1)
+#' @description `shape_check` checks whether `X` is a matrix representing
+#' a column vector (i.e., a matrix with 1 column). If not, `shape_check` attempts
+#' to coerce the user provided `X` to a matrix with 1 column. If this cannot be done,
+#' an error is raised.
+#' @param X Object to check and (if possible) coerce to a single column matrix
+#' @return A single column matrix
+#' @details This is a utility function written to check the input `X` for the
+#' functions `atrous_dwt` and `mo_dwt`.
 shape_check <- function(X) {
 
   if (is.vector(X)) {
